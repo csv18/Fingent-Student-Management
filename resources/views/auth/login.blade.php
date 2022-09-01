a116e715eb1622bec1836f6c507ba584b6225c2e
vignesh <vignesh@e-drona.in>
2022-09-01 15:05:17 +0530

    first-commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -78,6 +78,19 @@
 	<script src="{{ asset('theme/alertifyjs/build/alertify.min.js')}}"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.blockUI/2.70/jquery.blockUI.js"></script>
 	<script src="theme/login/js/login.js"></script>
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/6307460037898912e965143c/1gba6itah';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
 </body>
 
 </html>
